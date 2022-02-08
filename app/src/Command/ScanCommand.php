6fa6318333f2521fc2d2396917911db4b4ba9072
Fabian Jocks <dev@jocks.io>
2022-02-08 20:38:14 +0100

    Renamed run command to scan command

---
+++ app/src/Command/ScanCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+  name: 'anonrelay:scan',
+  description: 'Scan and apply redirection rules to all inboxes',
+)]
+class ScanCommand extends Command {
+  protected function execute(InputInterface $input, OutputInterface $output): int {
+    $io = new SymfonyStyle($input, $output);
+    
+    $io->success('You have successfully scanned all inboxes!');
+    
+    return Command::SUCCESS;
+  }
+}
