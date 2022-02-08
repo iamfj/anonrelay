18f126ae65755ef997a2a046df4cc506b6ffad72
Fabian Jocks <dev@jocks.io>
2022-02-08 21:44:23 +0100

    Added parameters from environment to services

--- app/src/Command/ScanCommand.php
+++ app/src/Command/ScanCommand.php
@@ -7,14 +7,23 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 #[AsCommand(
   name: 'anonrelay:scan',
   description: 'Scan and apply redirection rules to the configured inbox',
 )]
 class ScanCommand extends Command {
+  public function __construct(private ParameterBagInterface $parameterBag) {
+    parent::__construct();
+  }
+  
   protected function execute(InputInterface $input, OutputInterface $output): int {
     $io = new SymfonyStyle($input, $output);
+    
+    $imapPath = $this->parameterBag->get('app.imap.path');
+    $imapUsername = $this->parameterBag->get('app.imap.username');
+    $imapPassword = $this->parameterBag->get('app.imap.password');
     
     $io->success('You have successfully scanned all inboxes!');
     
